207261b00c7888679a4e243e7cb489aa3bc9dd49
Lucia Sanchez Fraile <luciasanchezfraile@lucias-MacBook-Pro.local>
2021-02-08 15:23:10 +0100

    added gulp rename dependecy

--- gulpfile.js
+++ gulpfile.js
@@ -2,14 +2,17 @@ var gulp = require('gulp');
 var sass = require('gulp-sass');
 var autoprefixer = require('gulp-autoprefixer');
 var cleanCSS = require('gulp-clean-css');
+var rename = require("gulp-rename");
  
 //run command "gulp sass" to compile, prefix and minimize Sass files. The final CSS will be storaged in 'CSS' folder.
 gulp.task('sass', function () {
     return gulp.src('./sass/styles.scss')
     .pipe(autoprefixer())
     .pipe(sass.sync().on('error', sass.logError))
+    .pipe(gulp.dest('./css'))
     .pipe(cleanCSS({compatibility: 'ie8'}))
-    .pipe(gulp.dest('./css'));
+    .pipe(rename({suffix: '.min'}))
+    .pipe(gulp.dest('./css/'));
 });
 
 //run command "gulp watch" to run sass command every time a change is saved on any Sass file 
