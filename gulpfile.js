fb585756a049ac67b4582c86eb5cebbf81caf7a2
Lucia Sanchez Fraile <luciasanchezfraile@lucias-MacBook-Pro.local>
2021-02-08 14:37:12 +0100

    compiling,prefixing and minimizing sass with gulp

---
+++ gulpfile.js
@@ -0,0 +1,19 @@
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var autoprefixer = require('gulp-autoprefixer');
+var cleanCSS = require('gulp-clean-css');
+ 
+sass.compiler = require('node-sass');
+ 
+gulp.task('sass', function () {
+    return gulp.src('./sass/styles.scss')
+    .pipe(autoprefixer())
+    .pipe(sass.sync().on('error', sass.logError))
+    .pipe(cleanCSS({compatibility: 'ie8'}))
+    .pipe(gulp.dest('./css'));
+});
+ 
+
+gulp.task('watch', function () {
+    gulp.watch('./sass/*.scss', gulp.series('sass'));
+});
